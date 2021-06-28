0ecfb4f3007dabbe3d06558ac038a9298e0c7de0
David Forero <davidforero717@gmail.com>
2021-06-28 09:08:47 -0500

    se crea la clase Conexion para la conexion de base de datos

---
+++ src/main/java/datos/Conexion.java
@@ -0,0 +1,48 @@
+
+package datos;
+
+import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+/**
+ *
+ * @author david
+ */
+public class Conexion {
+    
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3307/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    public static final String JDBC_USER ="root";
+    public static final String JDBC_PASSWORD ="7227";
+    
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);
+        return ds;
+    }
+    
+    public static Connection getConnection() throws SQLException {
+    return getDataSource().getConnection();
+    }
+    
+    public static void close(ResultSet rs){
+    
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+     public static void close(Connection conn){
+    
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+}
