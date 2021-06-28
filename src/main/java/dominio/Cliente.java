003e645f982bcf54623dbe83320805952b4bac63
David Forero <davidforero717@gmail.com>
2021-06-28 09:58:07 -0500

    Se agrega clase Cliente

---
+++ src/main/java/dominio/Cliente.java
@@ -0,0 +1,16 @@
+package dominio;
+
+/**
+ *
+ * @author david
+ */
+public class Cliente {
+
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private double saldo;
+    
+}
